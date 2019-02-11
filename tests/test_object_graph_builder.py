2580ea7d78818085eaa41f4641d8de8b502ec1dc
Omar Shaban <omars@php.net>
2019-02-11 14:18:16 +0100

    add list based method tests

--- tests/test_object_graph_builder.py
+++ tests/test_object_graph_builder.py
@@ -1,9 +1,21 @@
 from unittest.mock import patch
 
 import pinject
+from pinject import BindingSpec
 from pinject.object_graph import ObjectGraph
+from pinject.object_providers import ObjectProvider
 
 from django_pinject import ObjectGraphBuilder
+
+
+class BindingSpec1(BindingSpec):
+    def configure(self, bind):
+        pass
+
+
+class BindingSpec2(BindingSpec):
+    def configure(self, bind):
+        pass
 
 
 class TestObjectGraphBuilder:
@@ -21,6 +33,15 @@ class TestObjectGraphBuilder:
         mocked_object_graph_method.assert_called_once_with(binding_specs=[], classes=[class_to_add], modules=[])
 
     @patch('django_pinject.pinject.new_object_graph')
+    def test_add_classes(self, mocked_object_graph_method):
+        builder = ObjectGraphBuilder()
+        classes = [TestObjectGraphBuilder, ObjectProvider]
+        builder.add_classes(classes)
+        builder.get_object_graph()
+
+        mocked_object_graph_method.assert_called_once_with(binding_specs=[], classes=classes, modules=[])
+
+    @patch('django_pinject.pinject.new_object_graph')
     def test_add_module(self, mocked_object_graph_method):
         builder = ObjectGraphBuilder()
         module_to_add = pinject
@@ -30,13 +51,31 @@ class TestObjectGraphBuilder:
         mocked_object_graph_method.assert_called_once_with(binding_specs=[], classes=[], modules=[module_to_add])
 
     @patch('django_pinject.pinject.new_object_graph')
+    def test_add_modules(self, mocked_object_graph_method):
+        builder = ObjectGraphBuilder()
+        modules_to_add = [pinject.decorators, pinject.bindings]
+        builder.add_modules(modules_to_add)
+        builder.get_object_graph()
+
+        mocked_object_graph_method.assert_called_once_with(binding_specs=[], classes=[], modules=modules_to_add)
+
+    @patch('django_pinject.pinject.new_object_graph')
     def test_add_binding_spec(self, mocked_object_graph_method):
         builder = ObjectGraphBuilder()
-        binding_spec_to_add = TestObjectGraphBuilder
+        binding_spec_to_add = BindingSpec1
         builder.add_binding_spec(binding_spec_to_add)
         builder.get_object_graph()
 
         mocked_object_graph_method.assert_called_once_with(binding_specs=[binding_spec_to_add], classes=[], modules=[])
+
+    @patch('django_pinject.pinject.new_object_graph')
+    def test_add_binding_specs(self, mocked_object_graph_method):
+        builder = ObjectGraphBuilder()
+        binding_specs_to_add = [BindingSpec1, BindingSpec2]
+        builder.add_binding_specs(binding_specs_to_add)
+        builder.get_object_graph()
+
+        mocked_object_graph_method.assert_called_once_with(binding_specs=binding_specs_to_add, classes=[], modules=[])
 
     def test_rebuild_when_tainted(self):
         builder = ObjectGraphBuilder()
