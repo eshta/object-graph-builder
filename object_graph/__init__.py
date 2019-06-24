b9f30948feb2919596fe4332507fa0889ecf3ef3
Omar Shaban <omars@php.net>
2019-06-24 17:01:46 +0200

    packaging

--- object_graph/__init__.py
+++ object_graph/__init__.py
@@ -1,3 +1,5 @@
-__version__ = '1.0.0'
+__version__ = '0.0.1'
 
 __all__ = ['builder']
+
+name = "object_graph"
--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ from codecs import open
 
 from setuptools import setup, find_packages
 
-
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 VERSION = __import__('object_graph').__version__
 
-
 with open(os.path.join(BASE_DIR, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
-
 
 setup(
     name='object-graph-builder',
@@ -27,10 +24,13 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=['decorator', 'pinject'],
     include_package_data=True,
-    keywords='django pinject dependency-injection di container builder',
+    keywords='Dependency injection, dependency-injection, dependency-injection-container,'
+             ' dependency-injection-framework, design-patterns, factory, ioc, ioc-container, python, pinject, '
+             'singleton',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries  '
     ],
 )
